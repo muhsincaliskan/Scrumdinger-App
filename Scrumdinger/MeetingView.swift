641f46a3bb9a9e1f8b8a94381eb554997e63c0b1
muhsincaliskan <muhsincaliskan96@gmail.com>
2022-12-05 01:31:31 +0300

    init

---
+++ Scrumdinger/MeetingView.swift
@@ -0,0 +1,41 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by Muhsin Çalışkan on 4.12.2022.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            ProgressView(value:5,total:15)
+            HStack{
+                VStack(alignment: .leading) {
+                    Text("Seconds Elapsed").font(.caption)
+                    Label("300",systemImage: "hourglass.bottomhalf.fill")
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Seconds Elapsed").font(.caption)
+                    Label("600",systemImage: "hourglass.tophalf.fill")
+                }
+
+            }
+            Circle().strokeBorder(lineWidth: 22)
+            HStack{
+                Text("Speaker 1 of 3")
+                
+            }
+        }
+        .padding(.top, 20.0)
+        
+    }
+}
+
+struct MeetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView()
+    }
+}
---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Muhsin Çalışkan on 4.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
